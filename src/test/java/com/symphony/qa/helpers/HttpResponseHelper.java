ba7e52fd993783446928c6b32c5d6d213f7aede0
Yanice CHERRAK <yanice.cherrak@symphony.com>
2021-01-08 16:04:29 +0100

    Add test step identifier for Allure

--- src/test/java/com/symphony/qa/helpers/HttpResponseHelper.java
+++ src/test/java/com/symphony/qa/helpers/HttpResponseHelper.java
@@ -1,10 +1,12 @@
 package com.symphony.qa.helpers;
 
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import org.testng.Assert;
 
 public class HttpResponseHelper {
 
+    @Step("Check that status code of {response} is {statusCode}")
     public static void assertStatusCode(Response response, int statusCode) {
         Assert.assertEquals(response.getStatusCode(), statusCode, "The expected status code should be: " + statusCode);
     }
--- src/test/java/com/symphony/qa/helpers/JsonDataHelper.java
+++ src/test/java/com/symphony/qa/helpers/JsonDataHelper.java
@@ -1,25 +1,31 @@
 package com.symphony.qa.helpers;
 
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 
 public class JsonDataHelper {
 
+    @Step("Check the equality of Actual: {actual} and Expected: {expected}")
     public static void assertEquals(String actual, String expected) {
         Assertions.assertEquals(actual, expected, "The expected result should be: " + expected);
     }
 
+    @Step("Check the equality of Actual: {actual} and Expected: {expected}")
     public static void assertEquals(int actual, int expected) {
         Assertions.assertEquals(actual, expected, "The expected result should be: " + expected);
     }
 
+    @Step("Check the equality of Actual: {actual} and Expected: {expected}")
     public static void assertEquals(boolean actual, boolean expected) {
         Assertions.assertEquals(actual, expected, "The expected result should be: " + expected);
     }
 
+    @Step("Check that Actual: {actual} is True")
     public static void assertTrue(boolean actual) {
         Assertions.assertTrue(actual, "The result should be: True");
     }
 
+    @Step("Check that Actual: {actual} is False")
     public static void assertFalse(boolean actual) {
         Assertions.assertFalse(actual, "The result should be: False");
     }
