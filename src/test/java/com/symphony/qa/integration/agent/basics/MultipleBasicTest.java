ef6347f9b0acc9deef08f9018a56520734059502
Yanice CHERRAK <yanice.cherrak@symphony.com>
2021-02-19 15:43:47 +0100

    remove base tests as not needed

--- src/test/java/com/symphony/qa/integration/agent/basics/MultipleBasicTest.java
+++ src/test/java/com/symphony/qa/integration/agent/basics/MultipleBasicTest.java
@@ -1,14 +1,10 @@
 package com.symphony.qa.integration.agent.basics;
 
-import com.symphony.qa.integration.BaseTest;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.junit.runner.RunWith;
 
 @Epic("Agent")
 @Feature("Basics")
@@ -18,6 +14,6 @@ import org.junit.runner.RunWith;
 //@CucumberOptions(
 //        features = {"src/test/resources/features/agent"}
 //)
-public class MultipleBasicTest extends BaseTest {
+public class MultipleBasicTest {
 
 }
