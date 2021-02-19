a05b3ca5b4bd8c242e4dc019b894506317a8c44b
Yanice CHERRAK <yanice.cherrak@symphony.com>
2021-02-19 15:42:18 +0100

    remove unneeded config

--- src/test/java/com/symphony/qa/integration/agent/basics/BasicTest.java
+++ src/test/java/com/symphony/qa/integration/agent/basics/BasicTest.java
@@ -1,15 +1,12 @@
 package com.symphony.qa.integration.agent.basics;
 
 import com.symphony.qa.helpers.actions.agent.basics.HealthCheckActions;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import io.qameta.allure.*;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.junit.runner.RunWith;
 
 @Epic("Agent")
 @Feature("Basics")
