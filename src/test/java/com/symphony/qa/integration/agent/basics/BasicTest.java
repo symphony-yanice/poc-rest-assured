49444a2ea5a9c6135142d95582b0ff618a10af4c
Yanice CHERRAK <yanice.cherrak@symphony.com>
2021-02-23 16:59:41 +0100

    remove cucumber tests

---
+++ src/test/java/com/symphony/qa/integration/agent/basics/BasicTest.java
@@ -0,0 +1,44 @@
+package com.symphony.qa.integration.agent.basics;
+
+import com.symphony.qa.helpers.actions.agent.basics.HealthCheckActions;
+import io.qameta.allure.*;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+@Epic("Agent")
+@Feature("Basics")
+@Story("Health_Checks")
+@Execution(ExecutionMode.CONCURRENT)
+public class BasicTest {
+
+    @Test
+    @DisplayName("test Health V2 with Actions (first)")
+    @Description("Given I retrieve a 200 response from the v2 health check endpoint")
+    public void testHealthV2First() {
+        HealthCheckActions healthCheckActions = new HealthCheckActions();
+        Response getHealthCheckResponse = healthCheckActions.IRetrieveV2HealthCheckEndpoint();
+        healthCheckActions.IAssertAllV2HealthCheckSuccessfulFields(getHealthCheckResponse);
+    }
+
+    @Test
+    @DisplayName("test Health V2 with Actions (second)")
+    @Description("Given I retrieve a 200 response from the v2 health check endpoint")
+    public void testHealthV2Second() {
+        HealthCheckActions healthCheckActions = new HealthCheckActions();
+        Response getHealthCheckResponse = healthCheckActions.IRetrieveV2HealthCheckEndpoint();
+        healthCheckActions.IAssertAllV2HealthCheckSuccessfulFields(getHealthCheckResponse);
+    }
+
+    @Test
+    @DisplayName("test Health V2 with Actions (third)")
+    @Description("Given I retrieve a 200 response from the v2 health check endpoint")
+    public void testHealthV2Third() {
+        HealthCheckActions healthCheckActions = new HealthCheckActions();
+        Response getHealthCheckResponse = healthCheckActions.IRetrieveV2HealthCheckEndpoint();
+        healthCheckActions.IAssertAllV2HealthCheckSuccessfulFields(getHealthCheckResponse);
+    }
+
+}
