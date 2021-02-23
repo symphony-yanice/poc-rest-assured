3b8db2495504da5b5eaaeda06901d1728176871d
Yanice CHERRAK <yanice.cherrak@symphony.com>
2021-02-23 11:18:37 +0100

    add cucumber to check the annotations

--- src/test/java/com/symphony/qa/integration/agent/basics/BasicTest.java
+++ src/test/java/com/symphony/qa/integration/agent/basics/BasicTest.java
@@ -1,27 +1,46 @@
 package com.symphony.qa.integration.agent.basics;
 
 import com.symphony.qa.helpers.actions.agent.basics.HealthCheckActions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import io.qameta.allure.*;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.runner.RunWith;
 
 @Epic("Agent")
 @Feature("Basics")
-@Story("Health Checks")
 @Execution(ExecutionMode.CONCURRENT)
+@RunWith(Cucumber.class)
+@CucumberOptions(features = { "src/test/java/features" },
+        plugin = { "pretty", "html:target/cucumber-html-reports",
+                "json:target/cucumber-html-reports/cucumber.json",
+                "rerun:target/failed_scenarios.txt" },
+                monochrome = true,
+                glue = { "definitions" }, tags = {"@SmokeTest"}
+)
 public class BasicTest {
-
-    @Test
-    @DisplayName("test Health V2")
-    @Description("This scenario is an example of Actions capabilities with cucumber")
-    @Issue("PLAT-10399")
-    public void testHealthV2() {
-        HealthCheckActions healthCheckActions = new HealthCheckActions();
-        Response getHealthCheckResponse = healthCheckActions.IRetrieveV2HealthCheckEndpoint();
-        healthCheckActions.IAssertAllV2HealthCheckSuccessfulFields(getHealthCheckResponse);
-    }
+//
+//    @Test
+//    @DisplayName("test Health V2")
+//    @Description("This scenario is an example of Actions capabilities")
+//    @Story("Health Checks")
+//    public void testHealthV2() {
+//        HealthCheckActions healthCheckActions = new HealthCheckActions();
+//        Response getHealthCheckResponse = healthCheckActions.IRetrieveV2HealthCheckEndpoint();
+//        healthCheckActions.IAssertAllV2HealthCheckSuccessfulFields(getHealthCheckResponse);
+//    }
+//
+//    @Test
+//    @DisplayName("test Health V2 with Cucumber")
+//    @Description("This scenario is an example of Cucumber capabilities")
+//    public void testHealthV2Cucumber() {
+//        HealthCheckActions healthCheckActions = new HealthCheckActions();
+//        Response getHealthCheckResponse = healthCheckActions.IRetrieveV2HealthCheckEndpoint();
+//        healthCheckActions.IAssertAllV2HealthCheckSuccessfulFields(getHealthCheckResponse);
+//    }
 
 }
